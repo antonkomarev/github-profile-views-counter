090d7ed04a6daf5d5e53be784138d3f272fdb9f5
antonkomarev <ell@cybercog.su>
2020-07-13 01:51:59 +0300

    Make it really a pixel

--- public/database-repository.php
+++ public/database-repository.php
@@ -66,7 +66,7 @@ try {
 
     $style = $_GET['style'];
     if ($style === 'pixel') {
-        echo '<svg xmlns="http://www.w3.org/2000/svg"/>';
+        echo '<svg xmlns="http://www.w3.org/2000/svg" width="1" height="1"/>';
         exit;
     }
 
