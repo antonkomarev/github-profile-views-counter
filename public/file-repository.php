d9067a8bb54b15e8d8cf80877195ce30104d3c62
antonkomarev <ell@cybercog.su>
2020-07-12 18:47:34 +0300

    Add error badge rendering

--- public/file-repository.php
+++ public/file-repository.php
@@ -14,17 +14,22 @@ declare(strict_types=1);
 use Dotenv\Dotenv;
 use Komarev\GitHubProfileViewsCounter\CounterImageRendererService;
 use Komarev\GitHubProfileViewsCounter\CounterFileRepository;
+use Komarev\GitHubProfileViewsCounter\ErrorImageRendererService;
 
 $basePath = realpath(__DIR__ . '/..');
 
 // Register The Auto Loader
 require $basePath . '/vendor/autoload.php';
 
+header('Content-Type: image/svg+xml');
+header('Cache-Control: max-age=0, no-cache, no-store, must-revalidate');
+
 try {
     $dotEnv = Dotenv::createImmutable($basePath);
     $dotEnv->safeLoad();
 
-    $counterSourceImagePath = $basePath . '/resources/views-count.svg';
+    $counterBadgePath = $basePath . '/resources/views-count-badge.svg';
+    $errorBadgePath = $basePath . '/resources/error-badge.svg';
 
     if ($_ENV['FILE_STORAGE_PATH'] === null) {
         $storagePath = $basePath . '/storage';
@@ -36,20 +41,22 @@ try {
     $username = trim($username);
 
     if ($username === '') {
-        throw new InvalidArgumentException('Query property `username` is missing');
+        $errorImageRenderer = new ErrorImageRendererService($errorBadgePath);
+
+        echo $errorImageRenderer->getImageWithMessage('Invalid query parameter: username');
+        exit;
     }
 
     $counterRepository = new CounterFileRepository($storagePath);
     $counterRepository->incrementCountByUsername($username);
     $count = $counterRepository->getCountByUsername($username);
 
-    $counterImageRenderer = new CounterImageRendererService($counterSourceImagePath);
+    $counterImageRenderer = new CounterImageRendererService($counterBadgePath);
     $counterImage = $counterImageRenderer->getImageWithCount($count);
-
-    header('Content-Type: image/svg+xml');
-    header('Cache-Control: max-age=0, no-cache, no-store, must-revalidate');
 
     echo $counterImage;
 } catch (Exception $exception) {
-    echo $exception->getMessage();
+    $errorImageRenderer = new ErrorImageRendererService($errorBadgePath);
+
+    echo $errorImageRenderer->getImageWithMessage($exception->getMessage());
 }
