84ac753adf097d1a3f3673133b6c89de3538ea3c
Anton Komarev <ell@cybercog.su>
2023-09-02 15:50:14 +0300

    Constant name consistency

--- src/Username.php
+++ src/Username.php
@@ -17,9 +17,8 @@ use Webmozart\Assert\Assert;
 
 final class Username
 {
-    private const MIN_LENGTH = 1;
-
-    private const MAX_LENGTH = 39;
+    private const LENGTH_MIN = 1;
+    private const LENGTH_MAX = 39;
 
     /**
      * RegEx adapted from https://github.com/shinnn/github-username-regex/blob/master/index.js
@@ -30,8 +29,8 @@ final class Username
 
     public function __construct(string $username)
     {
-        Assert::minLength($username, self::MIN_LENGTH);
-        Assert::maxLength($username, self::MAX_LENGTH);
+        Assert::minLength($username, self::LENGTH_MIN);
+        Assert::maxLength($username, self::LENGTH_MAX);
         Assert::regex($username, self::REGEX, 'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.');
 
         $this->username = strtolower($username);
